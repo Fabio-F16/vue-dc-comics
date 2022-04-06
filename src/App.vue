b4bb4ca571c8956811e6e4418c82294f0d31c2c5
Fabio-F16 <fipsfabio95@gmail.com>
2022-04-06 15:09:21 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HeaderOk />
+    <MainOk />
+    <FooterOk />
+  </div>
+</template>
+
+<script>
+import HeaderOk from "./components/HeaderOk.vue";
+import MainOk from "./components/MainOk.vue";
+import FooterOk from "./components/FooterOk.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderOk,
+    MainOk,
+    FooterOk,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
